ed76b582b6b7984a3c1737f9f6b96e3d968fb4f2
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2023-09-03 23:38:01 -0300

    atividade1

--- atividade1/verifica_numero_primo.c
+++ atividade1/verifica_numero_primo.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main(int argc, char* argv[]){
-    int num, i;
-
-    num = atoi(argv[1]);
-
-    if(num <= 1){
-        printf("0");
-    }else{
-        for(i=3; i < num; i+=2){
-            if(num%i==0){
-                printf("0");
-                break;
-            }
+bool ehPrimo(int numero) {
+    if (numero <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= numero; i++) {
+        if (numero % i == 0) {
+            return false;
         }
-        if(i==num){
-            printf("1");
-        }
+    }
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <numero>\n", argv[0]);
+        return 1;
+    }
+
+    int numero = atoi(argv[1]);
+
+    if (ehPrimo(numero)) {
+        printf("1 - O número %d é primo.\n", numero);
+    } else {
+        printf("0 - O número %d não é primo.\n", numero);
     }
 
     return 0;
